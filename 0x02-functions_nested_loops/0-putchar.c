600ea1d4ff29ca05ec1d56fe0ee5ff4c8a7f11fe
Swiftpixie <er.rumalak@gmail.com>
2023-09-12 14:32:53 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.n"
+/**
+ * main - Entry point
+ * Return: Always 0
+*/
+int main(void)
+{
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
+return (0);
+}
