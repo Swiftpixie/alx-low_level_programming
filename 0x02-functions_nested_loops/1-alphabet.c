f17ad267e01263ab24eabf6570cefb55151f34d6
Swiftpixie <er.rumalak@gmail.com>
2023-09-13 04:47:22 -0700

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,18 +6,10 @@
  *
  * Return: Always 0 (success)
  */
-void print_alphabet(void);
-int main(void)
-{
-print_alphabet();
-return (0);
-}
 void print_alphabet(void)
 {
-int i;
-for (i = 97; i <= 122; i++)
-{
+char i;
+for (i = 'a' ; i <= 'z' ; i++)
 putchar(i);
-}
 putchar('\n');
 }
--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,12 +1,13 @@
-#include "main.h"
+#include <unistd.h>
 
 /**
- * main - check the code
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
- * Return: Always 0.
+ * Return: on success 1.
+ * on error, -1 is returned, and errno is set appropriately.
  */
-int main(void)
+int _putchar(char c)
 {
-    print_alphabet();
-    return (0);
+return (write(1, &c, 1));
 }
