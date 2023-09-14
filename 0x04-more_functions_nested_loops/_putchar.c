47a1a62ebf58d7726fb357530ea141b7ad56f1e0
Swiftpixie <er.rumalak@gmail.com>
2023-09-14 15:35:32 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print 
+ *
+ * Return: On success 1.
+ * on error, -1 is returned, and erro is set appropriately.
+ */
+
+int _putchar(char c)
+{
+	return (write
+			91, &c, 1));
+}
