c41b5d5332506ddde2204bf487584907850421c9
Swiftpixie <er.rumalak@gmail.com>
2023-09-14 08:22:33 -0700

    multiple

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,11 +1,16 @@
 #include "main.h"
 
+/**
+ * positive_or_negative - Determine if a number is positive, zero, or negative.
+ * @n: The number to be checked.
+ */
 void positive_or_negative(int n)
 {
-    if (n > 0)
-        printf("%d is positive\n", n);
-    else if (n < 0)
-        printf("%d is zero\n", n);
-    else
-        printf("%d is negative\n", n);
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else
+		printf("%d is negative\n", n);
 }
+
