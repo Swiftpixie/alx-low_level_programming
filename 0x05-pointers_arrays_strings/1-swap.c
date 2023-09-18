47d826c2e78a8c8b31fbfc1de0e8c95b5eb78805
Swiftpixie <er.rumalak@gmail.com>
2023-09-18 16:42:11 -0700

    Don't swap horses in crossing a stream

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * sawp_int - take in two variable integres and swaps them
+ * @author Anisah
+ *@a: swaps and stores adress of b
+ *@b: swaps and stores adress of a
+ * Return: 0
+ */
+
+void swap_int(int *a, int *b)
+{
+int change;
+change = *b;
+*b = *a;
+*a = change;
+}
