34c036e340115a71b1daeab16bfc2a2a4cb356ad
Swiftpixie <er.rumalak@gmail.com>
2023-10-12 19:09:13 -0700

    task1

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,28 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * op_add - sum two numbers 
+ * @a: fisrt number
+ * @b: second number
+ *
+ * Return: sum of the two numbers
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtract two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: difference of a and b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
