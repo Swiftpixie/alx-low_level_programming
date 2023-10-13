3ac262057ec1a5b5498f07bd0906eccf51d01ff0
Swiftpixie <er.rumalak@gmail.com>
2023-10-12 18:18:56 -0700

    task

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+/**
+ * int_index - search for an integer
+ * @array: target array holding int
+ * @size: arraysize
+ * @cmp: function pointer to compare search int
+ *
+ * Return: int element match, - if size <=0
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || cmp == NULL)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (1);
+	}
+	return (-1);
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -3,6 +3,7 @@
 
 #include <stddef.h>
 void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
 void print_name(char *name, void (*f)(char *));
 void print_name_as_is(char *name);
 void print_name_uppercase(char *name);
