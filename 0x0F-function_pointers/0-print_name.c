9539ca5ff9eeacc4609179d820a529de683ce636
Swiftpixie <er.rumalak@gmail.com>
2023-10-13 04:34:31 -0700

    What's my name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,11 +5,13 @@
  * print_name - prints name
  * @name: name to print
  * @f: function pointer
+ * Return: nothing
  */
 
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
 		return;
+
 	f(name);
 }
--- 0x0F-function_pointers/main.h
+++ 0x0F-function_pointers/main.h
@@ -1,7 +1,7 @@
 #include <stddef.h>
 #ifndef MAIN_H
 #define MAIN_H
-
+int (*get_op_func(char *s))(int, int);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
